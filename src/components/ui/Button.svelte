f03c56743a06aa01de5f8bb878aabaf14231d5ca
theomaro <mathewmaro02@gmail.com>
2023-12-07 09:18:11 +0300

    add ui-component (Button)

---
+++ src/components/ui/Button.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let onClickHandler: (
+    event: MouseEvent & { currentTarget: EventTarget & HTMLButtonElement }
+  ) => void;
+
+  export let text: string = "";
+  export let styles: string = "";
+</script>
+
+<button on:click={onClickHandler} class={styles}
+  >{text}
+  <slot />
+</button>
