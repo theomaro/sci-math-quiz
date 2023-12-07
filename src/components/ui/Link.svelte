2a31554b8f528ab1419921df399fc5b264de1f11
theomaro <mathewmaro02@gmail.com>
2023-12-07 09:25:58 +0300

    add ui-component (Link)

---
+++ src/components/ui/Link.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let href: string = "/";
+  export let text: string = "";
+  export let styles: string = "";
+</script>
+
+<a
+  {href}
+  class={`p-2 inline-block w-full hover:bg-red-50 hover:rounded md:hover:rounded-none capitalize ${styles}`}
+>
+  {text}
+  <slot />
+</a>
