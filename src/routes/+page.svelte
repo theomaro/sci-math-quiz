a5b2dffaef684894fcf12ef0c7a02737cc0d5daf
theomaro <mathewmaro02@gmail.com>
2023-12-07 06:29:52 +0300

    update document title & favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,7 @@
 <script lang="ts"></script>
 
 <svelte:head>
-  <title>Svelte Template</title>
+  <title>SciMathQ</title>
 </svelte:head>
 
-<h1 class="underline text-2xl font-medium">
-  Svelte <i class="ri-svelte-line"></i> Template
-</h1>
+<h1 class="underline text-2xl font-medium">SciMathQ</h1>
