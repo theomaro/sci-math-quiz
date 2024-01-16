c6e147d497959e221cd88f21fd982829181d8786
theomaro <theo.pombe@gmail.com>
2024-01-16 20:05:12 +0300

    add sidebar on teacher

---
+++ src/routes/teacher/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Sidebar from "../../components/teacher/Sidebar.svelte";
+</script>
+
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.6.0/remixicon.min.css"
+  />
+</svelte:head>
+
+<div class="grid grid-cols-12 max-w-7xl px-6 mx-auto">
+  <Sidebar />
+
+  <main class="col-span-10 shadow-lg rounded-e p-6 bg-white">
+    <slot />
+  </main>
+</div>
---
+++ src/routes/teacher/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+
+  let currentURL: string = "";
+</script>
+
+<svelte:head>
+  <title>Dashboard</title>
+</svelte:head>
