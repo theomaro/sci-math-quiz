a5c5da71b9a89e426d83f3118d757ba5fc8b19c7
theomaro <theo.pombe@gmail.com>
2024-01-20 07:30:41 +0300

    make login/register form responsive

--- src/components/ui/LoginForm.svelte
+++ src/components/ui/LoginForm.svelte
@@ -3,43 +3,43 @@
   export let pageTitle: string;
 </script>
 
-<div class="bg-slate-800 min-h-screen flex justify-center items-center">
+<div class="bg-slate-00 min-h-screen flex justify-center items-center">
   <section class="shadow-xl rounded-lg border border-gray-200 bg-gray-50 p-8">
-    <div class="text-center space-y-2 mb-4">
-      <h1 class="text-4xl capitalize">{pageTitle} login</h1>
-      <p class="text-lg text-slate-600">
+    <div class="text-center space-y-1 mb-8">
+      <h1 class="text-2xl md:text-3xl capitalize">{pageTitle} login</h1>
+      <p class="text-sm md:text-base text-slate-600">
         Please enter your credentials to login!
       </p>
     </div>
 
-    <form action="" class="flex flex-col gap-6 p-8 w-[32rem]">
-      <div class="flex flex-col gap-2 text-slate-800">
-        <label for="" class="text-base">Username:</label>
+    <form action="" class="flex flex-col gap-5">
+      <div class="flex flex-col gap-1 text-slate-800">
+        <label for="" class="text-sm">Username:</label>
         <input
           type="text"
-          class="py-2 px-3 text-lg border border-slate-400 rounded-lg outline-none"
+          class="py-1 px-3 w-80 text-lg border border-slate-400 rounded-md outline-none"
         />
       </div>
 
-      <div class="flex flex-col gap-2 text-slate-800">
-        <label for="" class="text-base">Enter Password:</label>
+      <div class="flex flex-col gap-1 text-slate-800">
+        <label for="" class="text-sm">Enter Password:</label>
         <input
           type="password"
-          class="py-2 px-3 text-lg border border-slate-400 rounded-lg outline-none"
+          class="py-1 px-3 w-80 text-lg border border-slate-400 rounded-md outline-none"
         />
       </div>
 
       <div class="flex justify-end">
         <button
           type="submit"
-          class="p-2.5 w-32 bg-emerald-700 text-slate-100 text-xl font-semibold capitalize rounded-lg"
+          class="py-2 w-24 bg-emerald-700 text-slate-100 text-base font-semibold capitalize rounded-md"
         >
           login
         </button>
       </div>
     </form>
 
-    <div class="flex flex-col items-start ps-8">
+    <div class="flex flex-col items-start text-sm">
       <p>Don't' have an account?</p>
       <button
         on:click={() => goto(`/${pageTitle.toLocaleLowerCase()}/register`)}
--- src/components/ui/RegisterForm.svelte
+++ src/components/ui/RegisterForm.svelte
@@ -3,66 +3,66 @@
   export let pageTitle: string;
 </script>
 
-<div class="bg-slate-800 min-h-screen flex justify-center items-center">
+<div class="bg-slate-00 min-h-screen flex justify-center items-center">
   <section class="shadow-xl rounded-lg border border-gray-200 bg-gray-50 p-8">
-    <div class="text-center space-y-2 mb-3">
-      <h1 class="text-4xl capitalize">{pageTitle} Registeration</h1>
-      <p class="text-lg text-slate-600">
-        Please enter your credentials to login!
+    <div class="text-center space-y-1 mb-8">
+      <h1 class="text-2xl md:text-3xl capitalize">{pageTitle} Registeration</h1>
+      <p class="text-sm md:text-base text-slate-600">
+        Please, enter your details!
       </p>
     </div>
 
-    <form action="" class="flex flex-col gap-6 p-8">
-      <div class="flex gap-8">
-        <div class="flex flex-col gap-2 text-slate-800">
-          <label for="" class="text-base">First name:</label>
+    <form action="" class="flex flex-col gap-5">
+      <div class="flex flex-col gap-3 md:flex-row md:gap-8">
+        <div class="flex flex-col gap-1 text-slate-800">
+          <label for="" class="text-sm">First name:</label>
           <input
             type="text"
-            class="py-2 px-3 w-96 text-lg border border-slate-400 rounded-lg outline-none"
+            class="py-1 px-3 w-80 text-lg border border-slate-400 rounded-md outline-none"
           />
         </div>
 
-        <div class="flex flex-col gap-2 text-slate-800">
-          <label for="" class="text-base">Last name:</label>
+        <div class="flex flex-col gap-1 text-slate-800">
+          <label for="" class="text-sm">Last name:</label>
           <input
             type="text"
-            class="py-2 px-3 w-96 text-lg border border-slate-400 rounded-lg outline-none"
+            class="py-1 px-3 w-80 text-lg border border-slate-400 rounded-md outline-none"
           />
         </div>
       </div>
 
-      <div class="flex gap-8">
-        <div class="flex flex-col gap-2 text-slate-800">
-          <label for="" class="text-base">Email:</label>
+      <div class="flex flex-col gap-3 md:flex-row md:gap-8">
+        <div class="flex flex-col gap-1 text-slate-800">
+          <label for="" class="text-sm">Email:</label>
           <input
             type="email"
-            class="py-2 px-3 w-96 text-lg border border-slate-400 rounded-lg outline-none"
+            class="py-1 px-3 w-80 text-lg border border-slate-400 rounded-md outline-none"
           />
         </div>
 
-        <div class="flex flex-col gap-2 text-slate-800">
-          <label for="" class="text-base">Username:</label>
+        <div class="flex flex-col gap-1 text-slate-800">
+          <label for="" class="text-sm">Username:</label>
           <input
             type="text"
-            class="py-2 px-3 w-96 text-lg border border-slate-400 rounded-lg outline-none"
+            class="py-1 px-3 w-80 text-lg border border-slate-400 rounded-md outline-none"
           />
         </div>
       </div>
 
-      <div class="flex gap-8">
-        <div class="flex flex-col gap-2 text-slate-800">
-          <label for="" class="text-base">Enter Password:</label>
+      <div class="flex flex-col gap-3 md:flex-row md:gap-8">
+        <div class="flex flex-col gap-1 text-slate-800">
+          <label for="" class="text-sm">Password:</label>
           <input
             type="password"
-            class="py-2 px-3 w-96 text-lg border border-slate-400 rounded-lg outline-none"
+            class="py-1 px-3 w-80 text-lg border border-slate-400 rounded-md outline-none"
           />
         </div>
 
-        <div class="flex flex-col gap-2 text-slate-800">
-          <label for="" class="text-base">Confirm Password:</label>
+        <div class="flex flex-col gap-1 text-slate-800">
+          <label for="" class="text-sm">Confirm Password:</label>
           <input
             type="password"
-            class="py-2 px-3 w-96 text-lg border border-slate-400 rounded-lg outline-none"
+            class="py-1 px-3 w-80 text-lg border border-slate-400 rounded-md outline-none"
           />
         </div>
       </div>
@@ -70,14 +70,14 @@
       <div class="flex justify-end">
         <button
           type="submit"
-          class="p-2.5 w-32 bg-emerald-700 text-slate-100 text-xl font-semibold capitalize rounded-lg"
+          class="py-2 w-28 bg-emerald-700 text-slate-100 text-base font-semibold capitalize rounded-md"
         >
           Register
         </button>
       </div>
     </form>
 
-    <div class="flex flex-col items-start ps-6">
+    <div class="flex flex-col items-start ps-1 text-sm">
       <p>Already have an account?</p>
       <button
         on:click={() => goto(`/${pageTitle.toLowerCase()}/login`)}
