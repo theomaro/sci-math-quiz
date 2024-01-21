4c9cd723205b587e56ae486b6077cdb43a7ef2d3
theomaro <theo.pombe@gmail.com>
2024-01-21 08:31:22 +0300

    disable form buttons

--- src/components/ui/LoginForm.svelte
+++ src/components/ui/LoginForm.svelte
@@ -80,6 +80,7 @@
 
       <div class="mb-8 mt-3">
         <button
+          disabled
           type="button"
           class="text-blue-700 capitalize w-full hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
           >login</button
