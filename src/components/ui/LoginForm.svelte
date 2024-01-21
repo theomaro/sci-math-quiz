a52a7bd4937cd3698ced4d1f90fba8708bbb0c72
theomaro <theo.pombe@gmail.com>
2024-01-21 08:26:53 +0300

    redesign forms

--- src/components/ui/LoginForm.svelte
+++ src/components/ui/LoginForm.svelte
@@ -18,30 +18,72 @@
       class="flex flex-col gap-5"
     >
       <div class="flex flex-col gap-1 text-slate-800">
-        <label for="username" class="text-sm">Username:</label>
-        <input
-          type="text"
-          name="username"
-          class="py-1 px-3 w-80 text-lg border border-slate-400 rounded-md outline-none"
-        />
+        <label
+          for="website-admin"
+          class="block mb-1 text-sm font-medium text-gray-900 dark:text-white"
+          >Username</label
+        >
+        <div class="flex">
+          <span
+            class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600"
+          >
+            <svg
+              class="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"
+              />
+            </svg>
+          </span>
+          <input
+            type="text"
+            id="website-admin"
+            class="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Bonnie Green"
+          />
+        </div>
       </div>
 
       <div class="flex flex-col gap-1 text-slate-800">
-        <label for="password" class="text-sm">Enter Password:</label>
-        <input
-          type="password"
-          name="password"
-          class="py-1 px-3 w-80 text-lg border border-slate-400 rounded-md outline-none"
-        />
+        <label
+          for="name"
+          class="block mb-1 text-sm font-medium text-gray-900 dark:text-white"
+          >Password</label
+        >
+        <div class="flex w-80">
+          <span
+            class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600"
+          >
+            <svg
+              class="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 50 50"
+            >
+              <path
+                d="M 25 2 C 17.832484 2 12 7.8324839 12 15 L 12 21 L 8 21 C 6.3550302 21 5 22.35503 5 24 L 5 47 C 5 48.64497 6.3550302 50 8 50 L 42 50 C 43.64497 50 45 48.64497 45 47 L 45 24 C 45 22.35503 43.64497 21 42 21 L 38 21 L 38 15 C 38 7.8324839 32.167516 2 25 2 z M 25 4 C 31.086484 4 36 8.9135161 36 15 L 36 21 L 14 21 L 14 15 C 14 8.9135161 18.913516 4 25 4 z M 8 23 L 42 23 C 42.56503 23 43 23.43497 43 24 L 43 47 C 43 47.56503 42.56503 48 42 48 L 8 48 C 7.4349698 48 7 47.56503 7 47 L 7 24 C 7 23.43497 7.4349698 23 8 23 z M 13 34 A 2 2 0 0 0 11 36 A 2 2 0 0 0 13 38 A 2 2 0 0 0 15 36 A 2 2 0 0 0 13 34 z M 21 34 A 2 2 0 0 0 19 36 A 2 2 0 0 0 21 38 A 2 2 0 0 0 23 36 A 2 2 0 0 0 21 34 z M 29 34 A 2 2 0 0 0 27 36 A 2 2 0 0 0 29 38 A 2 2 0 0 0 31 36 A 2 2 0 0 0 29 34 z M 37 34 A 2 2 0 0 0 35 36 A 2 2 0 0 0 37 38 A 2 2 0 0 0 39 36 A 2 2 0 0 0 37 34 z"
+              ></path>
+            </svg>
+          </span>
+          <input
+            type="text"
+            id="name"
+            class="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
       </div>
 
-      <div class="flex justify-end">
+      <div class="mb-8 mt-3">
         <button
-          type="submit"
-          class="py-2 w-24 bg-emerald-700 text-slate-100 text-base font-semibold capitalize rounded-md"
+          type="button"
+          class="text-blue-700 capitalize w-full hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+          >login</button
         >
-          login
-        </button>
       </div>
     </form>
 
--- src/components/ui/RegisterForm.svelte
+++ src/components/ui/RegisterForm.svelte
@@ -19,72 +19,139 @@
     >
       <div class="flex flex-col gap-3 md:flex-row md:gap-8">
         <div class="flex flex-col gap-1 text-slate-800">
-          <label for="firstname" class="text-sm">First name:</label>
-          <input
-            type="text"
-            name="firstname"
-            class="py-1 px-3 w-80 text-lg border border-slate-400 rounded-md outline-none"
-          />
-        </div>
-
-        <div class="flex flex-col gap-1 text-slate-800">
-          <label for="lastname" class="text-sm">Last name:</label>
-          <input
-            type="text"
-            name="lastname"
-            class="py-1 px-3 w-80 text-lg border border-slate-400 rounded-md outline-none"
-          />
-        </div>
-      </div>
-
-      <div class="flex flex-col gap-3 md:flex-row md:gap-8">
-        <div class="flex flex-col gap-1 text-slate-800">
-          <label for="email" class="text-sm">Email:</label>
-          <input
-            type="email"
-            name="email"
-            class="py-1 px-3 w-80 text-lg border border-slate-400 rounded-md outline-none"
-          />
-        </div>
-
-        <div class="flex flex-col gap-1 text-slate-800">
-          <label for="username" class="text-sm">Username:</label>
-          <input
-            type="text"
-            name="username"
-            class="py-1 px-3 w-80 text-lg border border-slate-400 rounded-md outline-none"
-          />
-        </div>
-      </div>
-
-      <div class="flex flex-col gap-3 md:flex-row md:gap-8">
-        <div class="flex flex-col gap-1 text-slate-800">
-          <label for="password" class="text-sm">Password:</label>
-          <input
-            type="password"
-            name="password"
-            class="py-1 px-3 w-80 text-lg border border-slate-400 rounded-md outline-none"
-          />
-        </div>
-
-        <div class="flex flex-col gap-1 text-slate-800">
-          <label for="confirm-password" class="text-sm">Confirm Password:</label
+          <label
+            for="name"
+            class="block mb-1 text-sm font-medium text-gray-900 dark:text-white"
+            >Your Name</label
           >
-          <input
-            type="password"
-            name="confirm-password"
-            class="py-1 px-3 w-80 text-lg border border-slate-400 rounded-md outline-none"
-          />
+          <div class="flex w-80">
+            <span
+              class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600"
+            >
+              <svg
+                class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"
+                />
+              </svg>
+            </span>
+            <input
+              type="text"
+              id="name"
+              class="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Bonnie Green"
+            />
+          </div>
+        </div>
+
+        <div class="flex flex-col gap-1 text-slate-800">
+          <label
+            for="email-address-icon"
+            class="block mb-1 text-sm font-medium text-gray-900 dark:text-white"
+            >Your Email</label
+          >
+          <div class="relative">
+            <div
+              class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none"
+            >
+              <svg
+                class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 16"
+              >
+                <path
+                  d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z"
+                />
+                <path
+                  d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z"
+                />
+              </svg>
+            </div>
+            <input
+              type="text"
+              id="email-address-icon"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="name@flowbite.com"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class="flex flex-col gap-3 md:flex-row md:gap-8">
+        <div class="flex flex-col gap-1 text-slate-800">
+          <label
+            for="name"
+            class="block mb-1 text-sm font-medium text-gray-900 dark:text-white"
+            >Password</label
+          >
+          <div class="flex w-80">
+            <span
+              class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600"
+            >
+              <svg
+                class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 50 50"
+              >
+                <path
+                  d="M 25 2 C 17.832484 2 12 7.8324839 12 15 L 12 21 L 8 21 C 6.3550302 21 5 22.35503 5 24 L 5 47 C 5 48.64497 6.3550302 50 8 50 L 42 50 C 43.64497 50 45 48.64497 45 47 L 45 24 C 45 22.35503 43.64497 21 42 21 L 38 21 L 38 15 C 38 7.8324839 32.167516 2 25 2 z M 25 4 C 31.086484 4 36 8.9135161 36 15 L 36 21 L 14 21 L 14 15 C 14 8.9135161 18.913516 4 25 4 z M 8 23 L 42 23 C 42.56503 23 43 23.43497 43 24 L 43 47 C 43 47.56503 42.56503 48 42 48 L 8 48 C 7.4349698 48 7 47.56503 7 47 L 7 24 C 7 23.43497 7.4349698 23 8 23 z M 13 34 A 2 2 0 0 0 11 36 A 2 2 0 0 0 13 38 A 2 2 0 0 0 15 36 A 2 2 0 0 0 13 34 z M 21 34 A 2 2 0 0 0 19 36 A 2 2 0 0 0 21 38 A 2 2 0 0 0 23 36 A 2 2 0 0 0 21 34 z M 29 34 A 2 2 0 0 0 27 36 A 2 2 0 0 0 29 38 A 2 2 0 0 0 31 36 A 2 2 0 0 0 29 34 z M 37 34 A 2 2 0 0 0 35 36 A 2 2 0 0 0 37 38 A 2 2 0 0 0 39 36 A 2 2 0 0 0 37 34 z"
+                ></path>
+              </svg>
+            </span>
+            <input
+              type="text"
+              id="name"
+              class="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+          </div>
+        </div>
+
+        <div class="flex flex-col gap-1 text-slate-800">
+          <label
+            for="name"
+            class="block mb-1 text-sm font-medium text-gray-900 dark:text-white"
+            >Confirm Password</label
+          >
+          <div class="flex w-80">
+            <span
+              class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600"
+            >
+              <svg
+                class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 50 50"
+              >
+                <path
+                  d="M 25 2 C 17.832484 2 12 7.8324839 12 15 L 12 21 L 8 21 C 6.3550302 21 5 22.35503 5 24 L 5 47 C 5 48.64497 6.3550302 50 8 50 L 42 50 C 43.64497 50 45 48.64497 45 47 L 45 24 C 45 22.35503 43.64497 21 42 21 L 38 21 L 38 15 C 38 7.8324839 32.167516 2 25 2 z M 25 4 C 31.086484 4 36 8.9135161 36 15 L 36 21 L 14 21 L 14 15 C 14 8.9135161 18.913516 4 25 4 z M 8 23 L 42 23 C 42.56503 23 43 23.43497 43 24 L 43 47 C 43 47.56503 42.56503 48 42 48 L 8 48 C 7.4349698 48 7 47.56503 7 47 L 7 24 C 7 23.43497 7.4349698 23 8 23 z M 13 34 A 2 2 0 0 0 11 36 A 2 2 0 0 0 13 38 A 2 2 0 0 0 15 36 A 2 2 0 0 0 13 34 z M 21 34 A 2 2 0 0 0 19 36 A 2 2 0 0 0 21 38 A 2 2 0 0 0 23 36 A 2 2 0 0 0 21 34 z M 29 34 A 2 2 0 0 0 27 36 A 2 2 0 0 0 29 38 A 2 2 0 0 0 31 36 A 2 2 0 0 0 29 34 z M 37 34 A 2 2 0 0 0 35 36 A 2 2 0 0 0 37 38 A 2 2 0 0 0 39 36 A 2 2 0 0 0 37 34 z"
+                ></path>
+              </svg>
+            </span>
+            <input
+              type="text"
+              id="name"
+              class="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+          </div>
         </div>
       </div>
 
       <div class="flex justify-end">
         <button
-          type="submit"
-          class="py-2 w-28 bg-emerald-700 text-slate-100 text-base font-semibold capitalize rounded-md"
+          type="button"
+          class="text-blue-700 capitalize hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+          >Register</button
         >
-          Register
-        </button>
       </div>
     </form>
 
